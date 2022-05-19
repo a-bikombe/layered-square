1e8304709776d17aee5044d0d1393a6a79cbb900
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-19 19:00:39 -0400

    init

---
+++ layered-square.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> <?= strtolower($pageTitle) ?> </title>
+	<link href="layered-square.css" rel="stylesheet">
+</head>
+
+<body>
+	<div class="box"></div>
+</body>
+
+</html>
